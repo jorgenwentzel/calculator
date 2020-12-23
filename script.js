d528ae4d74c35117ccd86934aec0ee173c392d71
Jorgen Wentzel <jorgenwentzel@gmail.com>
2020-12-24 00:48:10 +0100

    completed layout and style of calculator. No functionality added.

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+/*
+Display:
+Neccessary functions:
+    display:
+    check if number to display is > 7 digits
+        if so, make font size smaller
+    display digit when pressed on screen or on keyboard
+    When operator is pressed  
+        save digits on display 
+        highlight operator
+        when next digit is pressed
+            clear display
+            clear highlight
+            display new digits
+        when sum-operator or new operator is pressed
+            perform operation on display-digits and saved digits
+            return result and show on display
+
+neccessary variables:
+    input 1
+    input 2
+            */   
+
+
