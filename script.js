c28ec2b925d28f9bf8b8f29cc3ab6e4ae67c1ca2
Jorgen Wentzel <jorgenwentzel@gmail.com>
2021-01-11 11:32:29 +0100

    partially working calculator, working on fixing bugs, details in code

--- script.js
+++ script.js
@@ -4,61 +4,35 @@
 const displayValue = document.querySelector("#display");
 const numButtons = document.querySelectorAll(".number-btn");
 const operatorButtons = document.querySelectorAll(".operator-btn");
-let ongoing = false;
 //const buttons = document.querySelectorAll('button');
 
 //Calculator memory
 var memory = {
     input1:null,
     input2:null, 
-    input3:null,
-    input4:null,
     operator:null,
-    sum:null
+    sum:null,
+    ongoing:false,
+    previousKey:null
 };
 
 function operate(input1, input2, operator){
     switch(operator) {
         case '+':
-            displayValue.textContent = add(input1, input2);
+            return parseFloat(input1) + parseFloat(input2);
             break;
         case '-':
-            displayValue.textContent = subtract(input1, input2);
+            return parseFloat(input1) - parseFloat(input2);
             break;
         case '*':
-            displayValue.textContent = multiply(input1, input2);
+            return parseFloat(input1) * parseFloat(input2);
             break;
         case '/':
-            displayValue.textContent = divide(input1, input2);
+            return parseFloat(input1) / parseFloat(input2);
             break;
         default:
-            displayValue.textContent = 'error';
+            return 'error';
     }
-}
-
-//Calculator operations
-function add(input1, input2) {
-    var in1 = memory.input1;
-    var in2 = memory.input2;
-    return (in1+in2);
-}
-
-function subtract(input1, input2) {
-        var in1 = memory.input1;
-        var in2 = memory.input2;
-        return (in1-in2);
-}
-
-function multiply(input1, input2) {
-        var in1 = memory.input1;
-        var in2 = memory.input2;
-        return (in1*in2);
-}
-
-function divide(input1, input2) {
-    var in1 = memory.input1;
-    var in2 = memory.input2;
-    return (in1/in2);
 }
 
 function lengthChecker() {
@@ -104,54 +78,101 @@ function clearDisplay() {
     enableNum();
 }
 
-function sum(){
-    if((memory.input1 !== null) && (memory.input2 !== null)) {
-        displayValue.textContent = operate(input1, input2, operator);
-    }
-}
-
 function clearAll() {
     displayValue.textContent = "";
-    memory.input1 = null
-    memory.input2 = null
-    memory.operator = null
-    memory.sum = null
+    memory.input1 = null;
+    memory.input2 = null;
+    memory.operator = null;
+    memory.previousKey = null;
     enableNum();
     enableOperators();
 }
 
+//comma-btn
+document.getElementById("comma-btn").addEventListener("click", () => {
+    let currentDisplay = displayValue.textContent;
+    if (currentDisplay.includes('.')) {
+        return;
+    }
+    else if(!currentDisplay.includes('.')) {
+        displayValue.textContent += '.';
+    }
+});
 
-    //Add event listeners to numbuttons
-    
-    numButtons.forEach((button) => {
-        button.addEventListener('click', () => {
-            enableOperators();
+
+//numbutton eventlisteners
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        enableOperators();
+        if(memory.previousKey === 'sum') {
+            clearDisplay();
             displayValue.textContent += button.textContent;
             lengthChecker();
             memory.input1 = displayValue.textContent;
-        });
-    }); 
+        }
+        if(memory.previousKey === null) {
+            displayValue.textContent += button.textContent;
+            lengthChecker();
+            memory.input1 = displayValue.textContent;
+        }
+        else if(memory.previousKey === 'operator') {
+            clearDisplay();
+            memory.previousKey = null;
+            displayValue.textContent += button.textContent;
+            lengthChecker();
+            memory.input2 = displayValue.textContent;
+        }
+    });
+}); 
     
     
-
-    //Add event listeners to operatorbuttons
-    operatorButtons.forEach((button) => {
-        button.addEventListener('click', () => {  
-            disableOperators();
-            if(memory.input2 === null) {
-                memory.input2 = memory.input1;
-            }
-            else if(memory.input2 !== null) {
-                displayValue.textContent = operate(memory.input1, memory.input2, button,textContent);
+//operatorbutton eventlisteners
+//BUG: displayValue over 10 resets calculations
+//BUG: multiplying by one and then pressing plus operator adds one to sum before operating
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        enableNum();
+        if (button.textContent === '=') {
+            if(memory.input1 !== null && memory.input2 !== null){
+                displayValue.textContent = operate(memory.input1, memory.input2, memory.operator);
                 memory.input1 = displayValue.textContent;
-                memory.input2 = null;
+                memory.previousKey = 'sum';
             }
-            
-            
-            
-        });
-    }); 
+            return;
+        }
+        else if(memory.previousKey === 'sum'){
+            disableOperators();
+            memory.operator = button.textContent;
+            memory.previousKey = 'operator';
+        }
+        
+        else if (memory.input2 === null) {
+            disableOperators();
+            memory.operator = button.textContent;
+            memory.previousKey = 'operator';
+        }
 
+        else if (memory.input2 !== null) {
+            disableOperators();
+            displayValue.textContent = operate(memory.input1, memory.input2, button.textContent);
+            memory.input1 = displayValue.textContent;
+            memory.operator = button.textContent;
+            memory.previousKey = 'operator';
+        }
+    })           
+});
+            
+               
+            
+    
+/*
+TODO
+-add comma functionality
+-fix bugs in operator functions
+-clean code
+    -make sure its to standard and simplified
+    -remove unused code
+*/
 
 
 
